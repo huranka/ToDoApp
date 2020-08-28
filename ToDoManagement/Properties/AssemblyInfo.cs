9c7478936b92b643b6ff0d16126d9463a7beec78
sugano <111111@gmail.com>
2020-08-28 22:01:20 +0900

    Propertyフォルダを追加

--- ToDoManagement/Properties/AssemblyInfo.cs
+++ ToDoManagement/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("ToDoアプリケーション")]
+[assembly: AssemblyTitle("ToDoManagement")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ToDoアプリケーション")]
+[assembly: AssemblyProduct("ToDoManagement")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- ToDoManagement/Properties/Resources.Designer.cs
+++ ToDoManagement/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ToDoアプリケーション.Properties
+namespace ToDoManagement.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace ToDoアプリケーション.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ToDoアプリケーション.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ToDoManagement.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
